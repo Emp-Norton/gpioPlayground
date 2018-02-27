b9bdb3892199e10eb98d36c15d4fddd7cecb7fad
root <root@Norton.localdomain>
2018-02-27 09:40:38 -0800

    initial commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export class App extends React.Component {
+	constructor(props) {
+		super(props);
+    this.state = {
+
+    }
+	}
+
+
+  render() {
+    return (
+      <div> 
+        <p> Hello! </p>
+      </div>
+      )
+  }
+
+}
---
+++ server/server.js
@@ -0,0 +1,15 @@
+'use strict'
+let express = require('express');
+let path = require('path');
+
+let app = express();
+app.use(express.static(path.join(__dirname, '/../public/')));
+
+
+app.get('/', (req, res) => {
+	res.send('hi');
+})
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+})
