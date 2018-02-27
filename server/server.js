23a3e6f1cbf3c89e92f34c71483ad2245b0cff19
James Jelenko <jamesjelenko@gmail.com>
2018-02-27 12:26:56 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,8 +7,13 @@ let app = express();
 app.use(express.static(path.join(__dirname, '/../client/dist/')));
 
 app.get('*', (req, res) => {
+	// change this to serve up whatever images are in a given directory
+	// use to interact with RPi camera, see what it's seeing (stream video?)
 	res.sendFile(path.join(__dirname, '/../client/dist/ml.jpeg'));
 })
+
+// add end points for LED interaction, piezo buzzer / pulse-width modulation slider 
+// load up 'training' images for facial recognition
 app.get('/', (req, res) => {
 	res.status(200).json('ok')
 })
