03851f20307c7be59ae13a88794d8d8fdd2e611a
Emp-Norton <jamesjelenko@gmail.com>
2018-02-27 12:14:46 -0800

    complete rudimentary server skeleton for ship to RPi

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ let express = require('express');
 let path = require('path');
 
 let app = express();
+
 app.use(express.static(path.join(__dirname, '/../client/dist/')));
 
-
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/../client/dist/ml.jpeg'));
+})
 app.get('/', (req, res) => {
-	res.send('hi');
+	res.status(200).json('ok')
 })
 
 app.listen(3000, () => {
