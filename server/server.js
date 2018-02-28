271fe3eecc7508d679940d58fe5770153546bc09
root <root@Norton.localdomain>
2018-02-28 10:28:41 -0800

    add notes for TODOs

--- server/server.js
+++ server/server.js
@@ -4,7 +4,12 @@ let path = require('path');
 
 let app = express();
 
-app.use(express.static(path.join(__dirname, '/../client/dist/')));
+app.use(express.static(path.join(__dirname, '/../client/dist/'))); // serving static images without doing the path join nonsense on line 10. taking image and serving it (connect server with node GPIO lib)
+// facial recognition
+// what am I looking at? (take pic and show on screen)
+// morse code messager
+// light-bright 
+// pwm slider for passive buzzer
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '/../client/dist/ml.jpeg'));
