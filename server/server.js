a0028edea3e13977b9139e52f76b7ef2eb102788
root <root@Norton.localdomain>
2018-03-01 14:08:40 -0800

    add endpoint for buzzer

--- server/server.js
+++ server/server.js
@@ -5,29 +5,45 @@ let path = require('path');
 let app = express();
 
 let Gpio = require('onoff').Gpio;
+
 let buzz = new Gpio(10, 'out');
 let red = new Gpio(17, 'out');
 let green = new Gpio(27, 'out');
 let blue = new Gpio(22, 'out');
+
 let leds = {
-        "red": red,
-        "blue": blue,
-        "green": green
+  "red": red,
+  "blue": blue,
+  "green": green
 }
 
 
-app.use(express.static(path.join(__dirname, '/../client/dist/'))); // serving static images without doing the path join nonsense on line 10. taking image and serving it (connect server with node GPIO lib)
+app.use(express.static(path.join(__dirname, '/../client/dist/')));
 
 app.get('/light/:color/:toggle', (req, res) => {
-        let color = leds[req.params.color];
-        let toggle = req.params.toggle;
-        toggle = toggle == "on" ? 1 : 0;
-        console.log(color, toggle);
-        color.writeSync(toggle);
-	res.setHeader("Access-Control-Allow-Origin", "*");
-        res.status(200).json("done")
+  let color = leds[req.params.color];
+  let toggle = req.params.toggle;
+  toggle = toggle == "on" ? 1 : 0;
+  color.writeSync(toggle);
+  res.setHeader("Access-Control-Allow-Origin", "*"); // move this to an options hash to avoid repetition 
+  res.status(200).json("done")
 }) 
+
+app.get('/buzzer/:string', (req, res) => {
+  let message = req.params.string;
+  console.log(message);
+
+  let disable = () => {
+    buzz.writeSync(0);
+  }
+
+  buzz.writeSync(1);
+  setTimeout(disable, 250);
+  res.setHeader("Access-Control-Allow-Origin", "*"); // move this to an options hash to avoid repetition 
+  res.status(200).json("done")
+})
+
                                                                           
 app.listen(3000, () => {
-	console.log('listening on port 3000');
+  console.log('listening on port 3000');
 })
