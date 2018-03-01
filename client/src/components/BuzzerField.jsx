3a969cb335b70bd51e5ff1d50e6596e4819696dd
root <root@Norton.localdomain>
2018-03-01 14:07:33 -0800

    implement buzzer component

--- client/src/components/BuzzerField.jsx
+++ client/src/components/BuzzerField.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import img from '../../dist/loudspeaker.png'
+
 
 export default class BuzzerField extends React.Component {
 	constructor(props) {
@@ -8,9 +11,14 @@ export default class BuzzerField extends React.Component {
 		}
 	}
 
+	buzz() {
+
+	}
+
 	render() {
 		return (
 			<div>
+				<img src={img} className="loudspeaker" />
 				<input type="text" id="buzzerInput" placeholder="Buzzer words!" />
 			</div>
 		)
