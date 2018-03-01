068981a7a5d5fb0fbf99f4af969dcd4881de25df
root <root@Norton.localdomain>
2018-03-01 14:23:45 -0800

    add onclick to fire buzzer

--- client/src/components/BuzzerField.jsx
+++ client/src/components/BuzzerField.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import img from '../../dist/loudspeaker.png'
+import $ from 'jquery'; // fix this to be the react way (update state)
 
 
 export default class BuzzerField extends React.Component {
@@ -12,13 +13,23 @@ export default class BuzzerField extends React.Component {
 	}
 
 	buzz() {
+    let message = $('#buzzerInput').value;
+    axios.get(`http://192.168.86.163:3000/buzzer/${message}`)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
 
 	}
 
 	render() {
 		return (
 			<div>
-				<img src={img} className="loudspeaker" />
+				<img src={img} className="loudspeaker" onClick={ () => {
+          this.buzz();
+        }} />
 				<input type="text" id="buzzerInput" placeholder="Buzzer words!" />
 			</div>
 		)
