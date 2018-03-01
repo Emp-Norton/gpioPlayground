88d0e7b6091e1659c04689616c8434687f14ab97
James Jelenko <jamesjelenko@gmail.com>
2018-03-01 19:31:46 +0000

    merge issue fix.

--- server/server.js
+++ server/server.js
@@ -1,28 +1,32 @@
+                                                                                                                    
 'use strict'
 let express = require('express');
 let path = require('path');
-
 let app = express();
 
-app.use(express.static(path.join(__dirname, '/../client/dist'))); // serving static images without doing the path join nonsense on line 10. taking image and serving it (connect server with node GPIO lib)
-// facial recognition
-// what am I looking at? (take pic and show on screen)
-// morse code messager
-// light-bright 
-// pwm slider for passive buzzer
+let Gpio = require('onoff').Gpio;
+let buzz = new Gpio(10, 'out');
+let red = new Gpio(17, 'out');
+let green = new Gpio(27, 'out');
+let blue = new Gpio(22, 'out');
+let leds = {
+        "red": red,
+        "blue": blue,
+        "green": green
+}
 
-// app.get('*', (req, res) => {
-// 	// change this to serve up whatever images are in a given directory
-// 	// use to interact with RPi camera, see what it's seeing (stream video?)
-	
-// })
 
-// add end points for LED interaction, piezo buzzer / pulse-width modulation slider 
-// load up 'training' images for facial recognition
-app.get('/test', (req, res) => {
-	res.status(200).json('ok')
-})
+app.use(express.static(path.join(__dirname, '/../client/dist/'))); // serving static images without doing the path join nonsense on line 10. taking image and serving it (connect server with node GPIO lib)
 
+app.get('/light/:color/:toggle', (req, res) => {
+        let color = leds[req.params.color];
+        let toggle = req.params.toggle;
+        toggle = toggle == "on" ? 1 : 0;
+        console.log(color, toggle);
+        color.writeSync(toggle);
+        res.status(200).json('done')
+}) 
+                                                                          
 app.listen(3000, () => {
 	console.log('listening on port 3000');
 })
